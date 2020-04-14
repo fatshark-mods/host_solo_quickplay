5be7356fac5f6ae23308de4c63dbc5807846af3a
Hans Sjunnesson <hans.sjunnesson@gmail.com>
2020-04-14 09:08:59 +0200

    Initial upload

---
+++ scripts/mods/hostQuickPlay/hostQuickPlay_localization.lua
@@ -0,0 +1,11 @@
+return {
+	mod_description = {
+		en = "host your solo quick play games"
+	},
+	keybind_toggle_name = {
+		en = "Keybind: enable / disable the mod"
+	},
+	noUI = {
+		en = "Show Checkboxes in the start-mission UI"
+	},
+}
